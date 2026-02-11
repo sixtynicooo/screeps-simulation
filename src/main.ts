d4a63d76e791429bcf40a7bfd2f7ad8ceb7d00dc
sixtynicooo <sixtynicooo@gmail.com>
2026-02-11 18:26:41 +0100

    Update creazione 1 creeps

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,4 @@
+import { runRoomManager } from "rooms/manager-rooms";
 import { ErrorMapper } from "utils/ErrorMapper";
 
 declare global {
@@ -30,12 +31,7 @@ declare const global: {
 // When compiling TS to JS and bundling with rollup, the line numbers and file names in error messages change
 // This utility uses source maps to get the line numbers and file names of the original, TS source code
 export const loop = ErrorMapper.wrapLoop(() => {
-  console.log(`Current game tick is ${Game.time}`);
+  // console.log(`Current game tick is ${Game.time} livello 1 gcl ${Game.gcl.level}`);
+  runRoomManager();
 
-  // Automatically delete memory of missing creeps
-  for (const name in Memory.creeps) {
-    if (!(name in Game.creeps)) {
-      delete Memory.creeps[name];
-    }
-  }
 });
