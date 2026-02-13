55129e5ad40ab7b89b71039776a2e0980d4f729e
sixtynicooo <sixtynicooo@gmail.com>
2026-02-13 19:35:14 +0100

    Update gestione work funzionante

--- src/rooms/strategy-1.ts/rule-harvester.ts
+++ src/rooms/strategy-1.ts/rule-harvester.ts
@@ -1,36 +1,34 @@
 export function activityHarvester(creeps:Creep[]) {
-    for(let creep of creeps){
-        console.log(creep.store.getFreeCapacity(),creep.store.getCapacity(),creep.memory.working)
-        if(creep.store.getFreeCapacity() > 0) {
-            const sources = creep.room.find(FIND_SOURCES);
-            if(creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
-                creep.moveTo(sources[0]);
+        for(let creep of creeps){
+            console.log(creep.store.getFreeCapacity(),creep.store.getCapacity(),creep.memory.working)
+            if(creep.store.getFreeCapacity()===creep.store.getCapacity() && !creep.memory.working) {
+                const sources = creep.room.find(FIND_SOURCES);
+                if(creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
+                    creep.moveTo(sources[0]);
+
+                }
+            } else if(creep.store.getFreeCapacity()===creep.store.getCapacity() && creep.memory.working ){
+                creep.memory.working=false
+            }
+            else if((creep.store.getFreeCapacity()>0 && !creep.memory.working )){
                 creep.memory.working=true
+            }else if(creep.memory.working && creep.store.getFreeCapacity()>0){
+                const sources = creep.room.find(FIND_SOURCES);
+                if(creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
+                    creep.moveTo(sources[0]);
+                }
+
+            }else if((creep.store.getFreeCapacity()===0 && !creep.memory.working )){
+            if(creep.transfer(Game.spawns.Spawn1, RESOURCE_ENERGY) === ERR_NOT_IN_RANGE) {
+                    creep.moveTo(Game.spawns.Spawn1);
+                    console.log('trasferisco indietro',creep.memory.working)
+                }
             }
-        }
-        else if(creep.store.getFreeCapacity()===0 && creep.memory.working) {
-            if(creep.transfer(Game.spawns.Spawn1, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
-                creep.moveTo(Game.spawns.Spawn1);
+            else if(creep.store.getFreeCapacity()===0 && creep.memory.working) {
+                if(creep.transfer(Game.spawns.Spawn1, RESOURCE_ENERGY) === ERR_NOT_IN_RANGE) {
+                    creep.moveTo(Game.spawns.Spawn1);
+                    console.log('trasferisco indietero',creep.memory.working)
             }
-        }
-        else if(creep.store.getFreeCapacity()>0 && creep.store.getFreeCapacity()===creep.store.getCapacity()) {
-            creep.memory.working=true
         }
     }
 }
-
-/* for(let creep in creeps){
- if(creep.store.getFreeCapacity() > 0) {
-            const sources = creep.room.find(FIND_SOURCES);
-            if(creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
-                creep.moveTo(sources[0]);
-            }
-        }
-        else if(Game.spawns.Spawn1.energy < Game.spawns.Spawn1.energyCapacity) {
-            if(creep.transfer(Game.spawns.Spawn1, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
-                creep.moveTo(Game.spawns.Spawn1);
-            }
-        }
-
-    }
- */
