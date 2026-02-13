df704f7649a13fa2db14a0b914aab1c6ca7bf4fd
sixtynicooo <sixtynicooo@gmail.com>
2026-02-13 19:43:35 +0100

    fix ora va

--- src/rooms/strategy-1.ts/rule-harvester.ts
+++ src/rooms/strategy-1.ts/rule-harvester.ts
@@ -5,9 +5,9 @@ export function activityHarvester(creeps:Creep[]) {
                 const sources = creep.room.find(FIND_SOURCES);
                     if(creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
                         creep.moveTo(sources[0]);
-                    }else if(creep.store.getFreeCapacity()>0){
-                         creep.memory.working=true
                     }
+                }else if(creep.store.getFreeCapacity()>0){
+                         creep.memory.working=true
                 }else if(creep.store.getFreeCapacity()===0){
                     if(creep.transfer(Game.spawns.Spawn1, RESOURCE_ENERGY) === ERR_NOT_IN_RANGE) {
                         creep.moveTo(Game.spawns.Spawn1)
